6d72976b6df797117be584867c54919268cd361d
David Pozsar <david.pozsar@megaweb.hu>
2013-07-02 15:08:02 +0200

    module added & enabled on install

--- sql/grid-facebooklogin/site/data.0-0.1.sql
+++ sql/grid-facebooklogin/site/data.0-0.1.sql
@@ -1,3 +1,8 @@
+-- insert default values for table: module
+
+INSERT INTO "module" ( "module", "enabled" )
+     VALUES ( 'Grid\FacebookLogin', TRUE );
+
 -- insert default values for table: user_right
 
 INSERT INTO "user_right" ( "label", "group", "resource", "privilege", "optional", "module" )
